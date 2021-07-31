133c543a746f865c0bd6bc583e12b56e45912b6a
Aditi <82038181+Aditi21aug@users.noreply.github.com>
2021-07-31 08:58:41 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var bg,form,system,code,security;
+var treasure;
+var score=0;
+var database;
+var q1="How many staircases are there at Hogwarts?"
+var q2="Who was the Gamekeeper of Hogwarts?"
+var q3="Who was known as the Brightest Witch of her age?"
+var q4="Who was the Godfather of Harry Potter?"
+var q5="What was another name of Lord Voldemort?"
+var q=[q1,q2,q3,q4,q5];
+var win;
+var rand=0;
+var Sound;
+var clapsound;
+function preload() {
+ 
+  
+  bg= loadImage("Images/Image 6.jpg");
+  win=loadImage("Images/Image 1.jpg");
+  Sound = loadSound("HarryPotterTheme.mp3")
+  clapsound = loadSound("Clapping Sound.mp3")
+}
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  database = firebase.database();
+  security = new Security();
+  system = new System();
+  code = new Code();
+  rand=0;
+  security.display(0);
+  Sound.play();
+ 
+  
+}
+
+function draw() {
+  background(bg);
+  
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+// add code for changing the background to the treasure background
+console.log(rand)
+if(score <5 )
+  security.display(score)
+
+  if(score === 5) {
+    clear()
+    background(win)
+    security.hide();
+    fill("black")
+    textSize(40);
+    text("Congrats You Won",250, 200);
+    clapsound.play();
+    Sound.pause();
+  }
+
+}
